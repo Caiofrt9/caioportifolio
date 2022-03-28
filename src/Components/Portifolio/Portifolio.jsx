e91e649b9fe2d8728f3d61f892e58ae9c12297b2
Caio F <silvacaio295@gmail.com>
2022-03-28 18:39:28 -0300

    r

--- src/Components/Portifolio/Portifolio.jsx
+++ src/Components/Portifolio/Portifolio.jsx
@@ -3,6 +3,7 @@ import './portifolio.css'
 
 import img from '../../img/projeto1.png'
 import img2 from '../../img/projeto2.png'
+import img3 from '../../img/projeto3.png'
 // s
 
 const Portifolio = () => {
@@ -11,17 +12,19 @@ const Portifolio = () => {
       <h5>My Recent Work</h5>
       <h2>Portfolioo</h2>
 
-      <div className='container portifolio__container'>
+    <div className='container portifolio__container'>
+
       <article className='portifolio__item'>
           <div className='portifolio__item-image'>
-            <img src={img} alt=""></img>  
+            <img src={img3} alt=""></img>  
           </div>
-          <h3>Christimas Website</h3>
+          <h3>Deliciouuss</h3>
           <div className='portfolio__item-cta'>
-          <a href='https://github.com/Caiofrt9/ChristmasWebsite' className='btn' target="_blank" rel="noreferrer">Github</a>
-          <a href='https://caiofrt9.github.io/ChristmasWebsite/' className='btn btn-primary' target="_blank" rel="noreferrer">Live Demo</a>
+          <a href='https://github.com/Caiofrt9/deliciouuss' className='btn' target="_blank" rel="noreferrer">Github</a>
+          <a href='https://astonishing-kitsune-18b50c.netlify.app' className='btn btn-primary' target="_blank" rel="noreferrer">Live Demo</a>
           </div>
-        </article>    
+        </article> 
+ 
 
         <article className='portifolio__item'>
           <div className='portifolio__item-image'>
@@ -33,6 +36,18 @@ const Portifolio = () => {
           <a href='https://expense-tracker-git-main-caiofrt9.vercel.app' className='btn btn-primary' target="_blank" rel="noreferrer">Live Demo</a>
           </div>
         </article>  
+
+        <article className='portifolio__item'>
+          <div className='portifolio__item-image'>
+            <img src={img} alt=""></img>  
+          </div>
+          <h3>Christimas Website</h3>
+          <div className='portfolio__item-cta'>
+          <a href='https://github.com/Caiofrt9/ChristmasWebsite' className='btn' target="_blank" rel="noreferrer">Github</a>
+          <a href='https://caiofrt9.github.io/ChristmasWebsite/' className='btn btn-primary' target="_blank" rel="noreferrer">Live Demo</a>
+          </div>
+        </article>   
+        
 
         {/* <article className='portifolio__item'>
           <div className='portifolio__item-image'>
